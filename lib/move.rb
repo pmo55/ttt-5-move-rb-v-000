ab649598fc9a67008795b0195558a2ab65506e9a
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-06-08 02:26:56 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,12 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(string)
+  index=string.to_i
+  board(index-1)
+end
+def move(board,x,player="X")
+  
+  
+end
+  
